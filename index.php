eb54186768e313e05249959bd2a94ddf209295ed
Ervin <ervin.sahbegovic@stu.ibu.edu.ba>
2022-03-31 22:17:46 +0200

    qw

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-init_set('display_errors', 1);
-init_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-required_once("rest/DAO/S2E.class.php");
+ echo "tassasaest";
 
- echo "test";
- $DAO new =
+
  ?>
