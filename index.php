aa5ddb963101fb905ea9500731887166bca28f1c
Ervin <ervin.sahbegovic@stu.ibu.edu.ba>
2022-03-31 22:06:59 +0200

    a

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+init_set('display_errors', 1);
+init_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+required_once("rest/DAO/S2E.class.php");
+
  echo "test";
+ $DAO new =
  ?>
