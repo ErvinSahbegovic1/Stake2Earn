9e33680f9f71f1fe96d7ad800f4793e7474ef031
Ervin <ervin.sahbegovic@stu.ibu.edu.ba>
2022-03-31 22:42:04 +0200

    31.3.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
- echo "test";
+require_once("rest/DAO/S2E.class.php");
+echo "test";
+$dao = new s2eDao();
 
 
  ?>
--- rest/DAO/S2E.class.php
+++ rest/DAO/S2E.class.php
@@ -1,15 +1,23 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "ervajekralj";
+class s2eDao {
+  private $conn;
+  /* constructor */
+  public function __contruct(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "ervajekralj";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=stake2earndb", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    try {
+      $this->conn = new PDO("mysql:host=$servername;dbname=stake2earndb", $username, $password);
+      // set the PDO error mode to exception
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected successfully";
+    } catch(PDOException $e)
+    throw $e;
+    }
+    
+  }
 }
+
 ?>
